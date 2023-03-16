a4ac6fcb4500c1c4e0ccb149e3e30e5387430f78
matiaspenida <120784873+matiaspenida@users.noreply.github.com>
2023-03-15 21:51:23 -0300

    primera escritura en el repositorio

---
+++ function.js
@@ -0,0 +1,4 @@
+document.getElementById('boton1').addEventListener('click', function() {
+    document.getElementById('texto1').innerHTML = "Soy una persona comprometida con el trabajo. Me desempeño bien en el área de la informatica, en especial la programacion web frontend y backend."
+}) 
+
